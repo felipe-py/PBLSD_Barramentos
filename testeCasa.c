4a9fafa4417f8223f0fe0764fa60de2f37516e0c
Lucas Gabriel <142092072+LucaasGy@users.noreply.github.com>
2024-05-28 15:56:18 -0300

    feat: Desenho da casa

--- testeCasa.c
+++ testeCasa.c
@@ -11,7 +11,7 @@
 #define LW_BRIDGE_SPAN 0x100
 
 //Função para habilitar escrita nas FIFOs
-void habilitarLeitura(volatile int *WRREG_PTR){
+void habilitaLeitura(volatile int *WRREG_PTR){
     *WRREG_PTR = 1;
     *WRREG_PTR = 0;
 }
@@ -33,7 +33,7 @@ void setSprite_WBR(volatile int *WRREG_PTR, volatile int *DATA_A_PTR, volatile i
 }
 
 //Função para editar (mudar cor) determinado bloco do background (80 x 60)
-void editBackground_WBM(volatile int *WRREG_PTR, volatile int *DATA_A_PTR, volatile int *DATA_B_PTR, , int bloco_x, int bloco_y, int b, int g, int r){
+void editBackground_WBM(volatile int *WRREG_PTR, volatile int *DATA_A_PTR, volatile int *DATA_B_PTR, int bloco_x, int bloco_y, int b, int g, int r){
     int bloco = bloco_y * 80 + bloco_x;
 
     *DATA_A_PTR = (bloco << 4) | 0b0010;
@@ -102,8 +102,8 @@ int main(void){
 
 //-----------------------------------------------------------------------------------------------------------------------//
 
-    //Verde claro (grama)
-    setCorBackground_WBR(WRREG_PTR, DATA_A_PTR, DATA_B_PTR, 1, 6, 2); 
+    ///Azul claro (ceu)
+    setCorBackground_WBR(WRREG_PTR, DATA_A_PTR, DATA_B_PTR, 6, 4, 3); 
 
 //-----------------------------------------------------------------------------------------------------------------------//
 /*
@@ -114,81 +114,76 @@ int main(void){
         for (int bloco_x = 0; bloco_x < 80; bloco_x++) {
             //Azul claro (ceu)
             editBackground_WBM(WRREG_PTR, DATA_A_PTR, DATA_B_PTR, bloco_x, bloco_y, 6, 2, 1);
-        }
-    }
+*/
 
 //-----------------------------------------------------------------------------------------------------------------------//
-/*
-
-    //Quadrado sol (amarelo)
-    setQuadrado_DP(WRREG_PTR, DATA_A_PTR, DATA_B_PTR, 0, 6, 6, 2, 3, 512, 0);
     
     //Triangulo sol (amarelo)
-    setTriangulo_DP(WRREG_PTR, DATA_A_PTR, DATA_B_PTR, 0, 6, 6, 3, 3, 512, 1);
+    setTriangulo_DP(WRREG_PTR, DATA_A_PTR, DATA_B_PTR, 0, 6, 6, 7, 53, 511, 0);
+
+    //Quadrado sol (amarelo)
+    setQuadrado_DP(WRREG_PTR, DATA_A_PTR, DATA_B_PTR, 0, 6, 6, 6, 60, 511, 1);
 
 //-----------------------------------------------------------------------------------------------------------------------//
-/*
-
-    //Quadrado parede Casa (branco)
-    setQuadrado_DP(WRREG_PTR, DATA_A_PTR, DATA_B_PTR, 7, 7, 7, 7, 240, 320, 2);
-    
-    //Triangulo teto Casa (marrom)
-    setTriangulo_DP(WRREG_PTR, DATA_A_PTR, DATA_B_PTR, 1, 2, 4, 7, 120, 320, 3);
-
-//-----------------------------------------------------------------------------------------------------------------------//
-/*
-
-    //Quadrado janela 1 (marrom)
-    setQuadrado_DP(WRREG_PTR, DATA_A_PTR, DATA_B_PTR, 1, 2, 4, 1, 210, 290, 4);
-
-    //Quadrado janela 2 (marrom)
-    setQuadrado_DP(WRREG_PTR, DATA_A_PTR, DATA_B_PTR, 1, 2, 4, 1, 210, 350, 5);
-
-//-----------------------------------------------------------------------------------------------------------------------//
-/*
-
-    //Quadrado porta base (marrom)
-    setQuadrado_DP(WRREG_PTR, DATA_A_PTR, DATA_B_PTR, 1, 2, 4, 1, 270, 320, 6);
-
-    //Quadrado porta meio (marrom)
-    setQuadrado_DP(WRREG_PTR, DATA_A_PTR, DATA_B_PTR, 1, 2, 4, 1, 290, 320, 7);
-
-    //Quadrado porta topo (marrom)
-    setQuadrado_DP(WRREG_PTR, DATA_A_PTR, DATA_B_PTR, 1, 2, 4, 1, 310, 320, 8);
-
-//-----------------------------------------------------------------------------------------------------------------------//
-/*
-
-    //Sprite arvore 1
-    setSprite_WBR(WRREG_PTR, DATA_A_PTR, DATA_B_PTR, 1, 20, 300, 4, 1);
-
-    //Sprite arvore 2
-    setSprite_WBR(WRREG_PTR, DATA_A_PTR, DATA_B_PTR, 1, 610, 300, 4, 2);
-
-//-----------------------------------------------------------------------------------------------------------------------//
-/*
-
-    //Sprite diamante 1
-    setSprite_WBR(WRREG_PTR, DATA_A_PTR, DATA_B_PTR, 1, 290, 210, 10, 3);
-
-    //Sprite diamante 2
-    setSprite_WBR(WRREG_PTR, DATA_A_PTR, DATA_B_PTR, 1, 350, 210, 10, 4);
-
-//-----------------------------------------------------------------------------------------------------------------------//
-/*
 
     //Sprite barra laranja
-    setSprite_WBR(WRREG_PTR, DATA_A_PTR, DATA_B_PTR, 1, 340, 290, 11, 5);
+    setSprite_WBR(WRREG_PTR, DATA_A_PTR, DATA_B_PTR, 1, 320, 445, 11, 1);
 
-//-----------------------------------------------------------------------------------------------------------------------//
-/*
+    //Sprite diamante 1
+    setSprite_WBR(WRREG_PTR, DATA_A_PTR, DATA_B_PTR, 1, 352, 355, 22, 2);
 
-    //Sprite tronco 1 marrom
-    setSprite_WBR(WRREG_PTR, DATA_A_PTR, DATA_B_PTR, 1, 10, 300, 16, 6);
+    //Sprite diamante 2
+    setSprite_WBR(WRREG_PTR, DATA_A_PTR, DATA_B_PTR, 1, 272, 355, 22, 3);
+
+    //Sprite arvore 1
+    setSprite_WBR(WRREG_PTR, DATA_A_PTR, DATA_B_PTR, 1, 100, 461, 4, 4);
+
+    //Sprite arvore 2
+    setSprite_WBR(WRREG_PTR, DATA_A_PTR, DATA_B_PTR, 1, 511, 461, 4, 5);
 
     //Sprite tronco 2 marrom
-    setSprite_WBR(WRREG_PTR, DATA_A_PTR, DATA_B_PTR, 1, 630, 300, 16, 7);
-*/
+    setSprite_WBR(WRREG_PTR, DATA_A_PTR, DATA_B_PTR, 1, 471, 463, 16, 6);
+
+    //Sprite tronco 1 marrom
+    setSprite_WBR(WRREG_PTR, DATA_A_PTR, DATA_B_PTR, 1, 140, 463, 16, 7);
+
+//-----------------------------------------------------------------------------------------------------------------------//
+    
+    //Quadrado janela 1 (marrom)
+    setQuadrado_DP(WRREG_PTR, DATA_A_PTR, DATA_B_PTR, 1, 2, 4, 2, 365, 360, 2);
+
+    //Quadrado janela 2 (marrom)
+    setQuadrado_DP(WRREG_PTR, DATA_A_PTR, DATA_B_PTR, 1, 2, 4, 2, 365, 280, 3);
+
+//-----------------------------------------------------------------------------------------------------------------------//
+
+    //Quadrado porta base (marrom)
+    setQuadrado_DP(WRREG_PTR, DATA_A_PTR, DATA_B_PTR, 1, 2, 4, 2, 465, 320, 4);
+
+    //Quadrado porta meio (marrom)
+    setQuadrado_DP(WRREG_PTR, DATA_A_PTR, DATA_B_PTR, 1, 2, 4, 2, 435, 320, 5);
+
+//-----------------------------------------------------------------------------------------------------------------------//
+
+    //Triangulo teto Casa (marrom)
+    setTriangulo_DP(WRREG_PTR, DATA_A_PTR, DATA_B_PTR, 1, 2, 4, 3, 299, 245, 6);
+    //Triangulo teto Casa (marrom)
+    setTriangulo_DP(WRREG_PTR, DATA_A_PTR, DATA_B_PTR, 1, 2, 4, 3, 299, 275, 7);
+    //Triangulo teto Casa (marrom)
+    setTriangulo_DP(WRREG_PTR, DATA_A_PTR, DATA_B_PTR, 1, 2, 4, 3, 299, 305, 8);
+    //Triangulo teto Casa (marrom)
+    setTriangulo_DP(WRREG_PTR, DATA_A_PTR, DATA_B_PTR, 1, 2, 4, 3, 299, 335, 9);
+        //Triangulo teto Casa (marrom)
+    setTriangulo_DP(WRREG_PTR, DATA_A_PTR, DATA_B_PTR, 1, 2, 4, 3, 299, 365, 10);
+        //Triangulo teto Casa (marrom)
+    setTriangulo_DP(WRREG_PTR, DATA_A_PTR, DATA_B_PTR, 1, 2, 4, 3, 299, 395, 11);
+
+//-----------------------------------------------------------------------------------------------------------------------//
+
+    //Quadrado parede Casa (branco)
+    setQuadrado_DP(WRREG_PTR, DATA_A_PTR, DATA_B_PTR, 7, 7, 7, 15, 400, 320, 12);
+
+//-----------------------------------------------------------------------------------------------------------------------//
 
     if (munmap(LW_virtual, LW_BRIDGE_SPAN) != 0) {
         printf("ERROR: munmap() failed...\n");
